fce8360096ed2e1d976671d5055b94c466c30c79
RuiJing714 <60809955+RuiJing714@users.noreply.github.com>
2021-03-12 09:47:58 +0800

    Update hi.java

--- src/hi.java
+++ src/hi.java
@@ -2,6 +2,5 @@ public class hi {
     public static void main(String[] args) {
         System.out.println("hiworld!");
         System.out.println("hihiworld");
-        System.out.println("12323");
     }
 }
